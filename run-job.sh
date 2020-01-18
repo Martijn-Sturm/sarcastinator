0229b61f564820eb0da63a0864b924d75e066b6e
Maarten van den Berg <m.b.j.vandenberg@students.uu.nl>
2020-01-18 22:01:46 +0100

    Job-definities voor SGE

---
+++ run-job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Dit is een script dat gebruikt kan worden met de Sun Grid Engine.
+# Regels die beginnen met #$ worden gebruikt als parameters voor de job:
+
+# Mail info over de job naar
+#$ -M m.b.j.vandenberg@students.uu.nl
+
+# Mail als de job (b)egint, (e)rrort en (s)topt
+#$ -m bes
+
+# Voer de job uit in de working directory waarin je was toen het script werd gesubmit
+#$ -cwd
+
+# Naam voor de job (wordt gebruikt in de logs en mails)
+#$ -N infombd_project_cascade_single_run
+
+# Draai alleen in "all.q", omdat dat ook op deze host draait en dus de voorbereide files in /scratch kan zien
+#$ -q all.q
+
+# Submit de job met `qsub prepare-job.sh`.
+
+# Bash-opties:
+#  -e: Cancel alles als er een commando faalt
+#  -u: Crash onmiddelijk als een onbekende variabele wordt gebruikt (geeft normaal de lege string)
+#  -x: Print alle uitgevoerde commando's op stderr
+#  -o pipefail: Faal ook als een geredirect commando faalt
+
+set -euxo pipefail
+
+source venv/bin/activate
+cd src
+PYTHONUNBUFFERED=1 python run_single.py
