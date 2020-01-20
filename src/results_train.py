b456a261b49b44e8b15e7dbe4c209206766dfad9
Sturm,M.J. (Martijn) <m.j.sturm@students.uu.nl>
2020-01-20 22:49:01 +0100

    Commit for Temporal network tune

--- src/results_train.py
+++ src/results_train.py
@@ -2,28 +2,43 @@ from contextlib import redirect_stdout
 import os
 
 
-def proc_results(result, model, filename, logger, **kwargs):
+def proc_results(result, model, filename, logger, param_dict, **kwargs):
     accs_train = result.history['accuracy']
     accs_val = result.history['val_accuracy']
 
     loss_train = result.history['loss']
     loss_val = result.history['val_loss']
 
+    max_acc_train = max(accs_train)
+    max_loss_train = max(loss_train)
+
+    max_acc_val = max(accs_val)
+    max_loss_val = max(loss_val)
+
+    n_params = model.count_params()
+
     # Summary logging
     logger.info(f"\nresults for: {filename}")
-    logger.info(f"Best training accuracy: {max(accs_train)}")
-    logger.info(f"Best training loss: {max(loss_train)}")
-    logger.info(f"Best validation accuracy: {max(accs_val)}")
-    logger.info(f"Best validation loss: {max(loss_val)}\n")
+    logger.info(f"Model params: {n_params}")
+    logger.info(f"Best training accuracy: {max_acc_train:.4}")
+    logger.info(f"Best training loss: {max_loss_train:.4}")
+    logger.info(f"Best validation accuracy: {max_acc_val:.4}")
+    logger.info(f"Best validation loss: {max_loss_val:.4}\n")
 
     os.makedirs("./results/", exist_ok=True)
     with open(f"./results/{filename}.txt", "w") as f:
         f.write(f"Model: {filename}\n\n")
 
         # Kwargs:
-        for key, value in kwargs.items():
+        for key, value in param_dict.items():
             f.write(f"{key} \t : \t {value}\n")
         f.write("\n\n")
+
+        # Kwargs:
+        if kwargs:
+            for key, value in kwargs.items():
+                f.write(f"{key} \t : \t {value}\n")
+            f.write("\n\n")
         # Training results
         f.write(f"for epoch nr: train acc - train loss - vali acc - vali loss \n")
         i = 1
@@ -35,3 +50,16 @@ def proc_results(result, model, filename, logger, **kwargs):
         f.write(f"\n\nModel summary:\n")
         with redirect_stdout(f):
             model.summary()
+
+    # Make output dictionary:
+    output_dict = param_dict
+    output_dict.update({
+        "acc train": max_acc_train,
+        "loss train": max_loss_train,
+        "acc val": max_acc_val,
+        "loss val": max_loss_val,
+        "name": filename,
+        "params": n_params
+    })
+
+    return(output_dict)
--- src/temporal.py
+++ src/temporal.py
@@ -3,11 +3,12 @@ from tcn import TCN
 from tensorflow.keras import Input, Model, Sequential
 from tensorflow.keras.activations import relu, sigmoid, softmax
 from tensorflow.keras.initializers import he_normal
-from tensorflow.keras.layers import Dense, InputLayer
+from tensorflow.keras.layers import Dense, InputLayer, Flatten, Concatenate, Dropout
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.metrics import Accuracy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.regularizers import l2
 
 from logger import get_logger
 
@@ -72,6 +73,123 @@ def tcn1(input_shape, logger,
 
     return model
 
+
+def tcn2(input_shape_x, input_shape_author, input_shape_topic,
+         logger, dense_layers=32, learn_rate=0.001,
+         init="he_normal",
+         num_filters=32, filter_sizes=[3], activation="relu",
+         padding="same", l2_float=0.0, dropout=0.0,
+         skips=True, dil_cor=0):
+    """Convolution Network WITH merging of other feature vectors
+    """
+    optimizer = Adam(learning_rate=learn_rate)
+    
+    param_dict = {
+        "num filters": num_filters,
+        "filter sizes": filter_sizes,
+        "padding": padding,
+        "activation": activation,
+        "n dense layer": dense_layers,
+        "l2 regu": l2_float,
+        "drop-out": dropout,
+        "kernel init": init,
+        "skip connections": skips,
+        "dilation correction": dil_cor,
+        "learn rate": learn_rate
+    }
+    # Check if the right shape was passed as argument
+    if len(input_shape_x) != 2:
+        raise Exception("Input shape needs to have 2 dimensions.",
+                        "input shape:", input_shape_x)
+
+    logger.info(f"input shape post (word/sent, word_emb_dims): {input_shape_x}")
+
+    # Input layer
+    i_x = Input(shape=input_shape_x)  # (words/sent, word_emb)
+    # embedding_size = input_shape[1]
+
+    # Calculate number of blocks:
+    def calc_dilations(filter_size, field, dil_cor=0):
+        import math
+        max_dil = field / filter_size
+        max_dil = math.ceil(math.log(max_dil) / math.log(2))
+        dil_list = [2**i for i in range(0, max_dil+1-int(dil_cor))]
+        return(dil_list)
+
+    # Convolutional layes
+    logger.info(f"Filter sizes: {filter_sizes}")
+    tcn_compl = []
+    for filter_size in filter_sizes:
+
+        # Determine dilation list:
+        dilation_list = calc_dilations(
+            filter_size=filter_size,
+            field=input_shape_x[1])
+        logger.info(f"Dilation list: {dilation_list}")
+
+        o_tcn = TCN(
+            nb_filters=num_filters,
+            kernel_size=filter_size,
+            dilations=dilation_list,
+            padding=padding,
+            use_skip_connections=skips,
+            dropout_rate=dropout,
+            activation=activation,
+            kernel_initializer=init,
+            use_batch_norm=False,
+            use_layer_norm=True,
+            return_sequences=False,
+            nb_stacks=1
+            )(i_x)
+        tcn_compl.append(o_tcn)
+    # Concatenate and flatten different convolutional
+    # Filter outputs
+    if len(filter_sizes) > 1:
+        tcn_compl = Concatenate()(tcn_compl)
+        o_x = Dropout(dropout)(tcn_compl)
+    else:
+        o_x = Dropout(dropout)(tcn_compl[0])
+    # o_x = Flatten()(o_x)
+    model_x = Model(inputs=i_x, outputs=o_x)
+
+    # input layer for author embeddings:
+    i_author = Input(shape=input_shape_author)
+    logger.info(f"input shape author embs: (author_emb_dims): {input_shape_author}")
+    # Input layer for topic embeddings:
+    i_topic = Input(shape=input_shape_topic)
+    logger.info(f"input shape topic embs: (topic_emb_dims): {input_shape_topic}")
+    # Combine these inputs
+    o_comb = Concatenate()([i_author, i_topic])
+
+    # Create small submodel
+    model_other = Model(
+        inputs=[i_author, i_topic],
+        outputs=o_comb)
+
+    # Combine feature vectors
+    features_comb = Concatenate()([model_x.output, model_other.output])
+
+    # Dense completely connected layer
+    dense = Dense(
+        units=dense_layers,
+        activation=activation,
+        kernel_initializer=init,
+        kernel_regularizer=l2(l2_float))(features_comb)
+    dense = Dropout(dropout)(dense)
+
+    # Output layer
+    o = Dense(
+        units=1,
+        activation="sigmoid"
+    )(dense)
+
+    # Define model
+    model_tot = Model(inputs=[model_x.input, model_other.input], outputs=[o])
+
+    # Compile model
+    model_tot.compile(
+        loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return(model_tot, param_dict)
 
 # check_model = tcn1((100, 300), logger=get_logger("temporal_check"))
 # check_model.summary()
